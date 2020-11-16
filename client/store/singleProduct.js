6506f25002128842f3f28c29c4e0c9a4964e5592
Elisa <elisalevet@hotmail.com>
2020-11-15 22:02:42 -0500

    feat: api and reducer for update

--- client/store/singleProduct.js
+++ client/store/singleProduct.js
@@ -1,10 +1,18 @@
 import axios from 'axios'
+import {bindActionCreators} from 'redux'
 
 const SET_SINGLE_PRODUCT = 'SET_SINGLE_PRODUCT'
+
+const UPDATE_PRODUCT = 'UPDATE_PRODUCT'
 
 const setSingleProduct = id => ({
   type: SET_SINGLE_PRODUCT,
   id
+})
+
+const updateProduct = product => ({
+  type: UPDATE_PRODUCT,
+  product
 })
 
 export const fetchSingleProduct = id => {
@@ -18,12 +26,40 @@ export const fetchSingleProduct = id => {
   }
 }
 
+export const fetchUpdateProduct = (
+  productId,
+  name,
+  description,
+  quantity,
+  price,
+  imageUrl,
+  category
+) => {
+  return async dispatch => {
+    try {
+      const {data} = await axios.put(`/api/products/${productId}`, {
+        name,
+        description,
+        quantity,
+        price,
+        imageUrl,
+        category
+      })
+      dispatch(updateProduct(data))
+    } catch (error) {
+      console.log('error fetching THUNK')
+    }
+  }
+}
+
 const initialState = {}
 
 export default function singleProduct(state = initialState, action) {
   switch (action.type) {
     case SET_SINGLE_PRODUCT:
       return action.id
+    case UPDATE_PRODUCT:
+      return {...state, ...action.product}
     default:
       return state
   }
