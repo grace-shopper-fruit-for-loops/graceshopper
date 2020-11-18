da96a2fd09e3ba5973bebf85149c401744b66eaf
Mackenzie Kroon <kroonmackenzie@gmail.com>
2020-11-17 21:37:17 -0600

    feat: checkout button

--- server/api/orders.js
+++ server/api/orders.js
@@ -60,36 +60,6 @@ router.post('/', async (req, res, next) => {
   }
 })
 
-// update isFulfilled to true
-// router.put('/:orderId', async (req, res, next) => {
-//   try {
-//     await Order.update(req.body, {
-//       where: {
-//         id: req.params.orderId
-//       },
-//       returning: true,
-//       plain: true
-//     })
-//     res.sendStatus(200)
-//   } catch (error) {
-//     next(error)
-//   }
-// })
-
-// router.put('/:orderId', async (req, res, next) => {
-//   try {
-//     console.log('REQ BODY IN ORDER', req.params.orderId)
-//     const updatedOrderDetails = await Order.findByPk(req.params.orderId)
-//     const data = {
-//       isFulfilled: req.body.isFulfilled
-//     }
-//     await updatedOrderDetails.update(data)
-//     res.send(updatedOrderDetails)
-//   } catch (error) {
-//     next(error)
-//   }
-// })
-
 router.put('/', async (req, res, next) => {
   try {
     const userId = await req.user.dataValues.id
